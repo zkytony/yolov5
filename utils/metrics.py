4326c5ac2f4843526ed43139e7779b1bd30ebb15
Kaiyu Zheng <kaiyutony@gmail.com>
2021-09-05 22:42:48 -0400

    smaller

--- utils/metrics.py
+++ utils/metrics.py
@@ -164,12 +164,12 @@ class ConfusionMatrix:
             array = self.matrix / ((self.matrix.sum(0).reshape(1, -1) + 1E-6) if normalize else 1)  # normalize columns
             array[array < 0.005] = np.nan  # don't annotate (would appear as 0.00)
 
-            fig = plt.figure(figsize=(14, 10), tight_layout=True)
+            fig = plt.figure(figsize=(17, 14), tight_layout=True)
             sn.set(font_scale=1.0 if self.nc < 50 else 0.8)  # for label size
             labels = (0 < len(names) < 99) and len(names) == self.nc  # apply names to ticklabels
             with warnings.catch_warnings():
                 warnings.simplefilter('ignore')  # suppress empty matrix RuntimeWarning: All-NaN slice encountered
-                sn.heatmap(array, annot=self.nc < 50, annot_kws={"size": 7}, cmap='Blues', fmt='.2f', square=True,
+                sn.heatmap(array, annot=self.nc < 50, annot_kws={"size": 5}, cmap='Blues', fmt='.2f', square=True,
                            xticklabels=names + ['background FP'] if labels else "auto",
                            yticklabels=names + ['background FN'] if labels else "auto").set_facecolor((1, 1, 1))
             fig.axes[0].set_xlabel('True')
